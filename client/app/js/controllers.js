0e8f772a6858211877c9fd773b031c0d6f9772ed
Rohith Salim <rohitsalim@gmail.com>
2012-10-06 18:46:56 -0400

    Testing if it works on Heroku

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -4,24 +4,37 @@
 
 
 function MyCtrl1($scope, $http) {
-	$http.get('venues/venues.json').success(function(data) {
-		console.log(data);
-   	 	$scope.venues = data;
-   	 	$scope.location = "Vegas";
-  	});
+
+  $http.jsonp('http://hidden-inlet-2627.herokuapp.com/place/NYC?callback=JSON_CALLBACK').
+  success(function(data, status, headers, config){
+    console.log(data)
+    $scope.venues = data["venues"];
+    console.log("Status " + status);
+  }).
+  error(function(data, status, headers, config) {
+
+  });
+
+//	$http.get('venues/venues.json').success(function(data) {
+//   	 	$scope.venues = data;
+//   	 	$scope.location = "Vegas";
+//  	});
 
 }
 
-function setBackgroundImage($scope){
-	var image = $scope.venue.img;
-	console.log(image);
+function get_instagram_image($scope, $http){
+  var instagram_client_id = "0790cbf24ff84eb2ab0f57660dacc016"
+  var instagram_client_secret = "055b5b7661564c2197c0b92d1105bbb1"
+  var id = 644104;
+  $http.jsonp("https://api.instagram.com/v1/locations/"+id+"/media/recent?client_id=0790cbf24ff84eb2ab0f57660dacc016&callback=JSON_CALLBACK").
+  success(function(object, status, headers, config){
+    var url = object["data"][0]["images"]["standard_resolution"]["url"];
+    console.log(url);
+    $scope.img = url;
+  }).
+  error(function(data, status, headers, config) {
 
-	$scope.mystyle = {
-        "background":"red",
-        "backgroundImage":"url("+image+")",
-        "backgroundSize":"220px 220px"
-
-    };
+  });
 }
 
 //MyCtrl1.$inject = [$scope, $http];
