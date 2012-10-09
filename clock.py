fc69899b193f97c9700680c07f3feb9e2a62d8e0
Madhav Bhagat <madhav89@gmail.com>
2012-10-08 21:59:45 -0400

    added initial clock process

---
+++ clock.py
@@ -0,0 +1,20 @@
+from apscheduler.scheduler import Scheduler
+
+sched = Scheduler()
+
+@sched.interval_schedule(hours=1)
+def foursquare_downloader():
+    print 'FSQ DOWNLOAD'
+
+@sched.interval_schedule(minutes=3)
+def timed_job():
+    print 'This job is run every three minutes.'
+
+@sched.cron_schedule(day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print 'This job is run every weekday at 5pm.'
+
+sched.start()
+
+while True:
+    pass
